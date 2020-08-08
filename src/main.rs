097215ebbdc2ad08d54192ba2bd4beeec8807add
Mark <mverleg.noreply@gmail.com>
2020-08-08 18:57:20 +0200

    Parse the command name and script to find
    Hand-coded argument parsing for small binary size and because most of
    it is dynamic.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use ::std::env::args;
+
+use crate::common::Res;
+use crate::params::parse_params;
+
+mod common;
+mod params;
+
 fn main() {
+    if let Err(err_msg) = run(args().collect()) {
+        eprintln!("Error! {}", err_msg);
+    };
     println!("Hello, world!");
+}
+
+fn run(arguments: Vec<String>) -> Res<()> {
+    let args = parse_params(arguments)?;
+    Ok(())  //TODO @mark: TEMPORARY! REMOVE THIS!
 }
---
+++ src/params.rs
@@ -0,0 +1,41 @@
+use ::std::collections::HashMap;
+use ::std::path::PathBuf;
+use ::std::str::FromStr;
+
+use crate::common::Res;
+
+#[derive(Debug)]
+pub struct Params {
+    pub command: String,
+    pub script: PathBuf,
+    pub bindings: HashMap<String, String>,
+}
+
+pub fn parse_params(arguments: Vec<String>) -> Res<Params> {
+    let command = match arguments.get(0) {
+        Some(cmd) => cmd.to_owned(),
+        None => return Err("did not find the command 'psy' was invoked with".to_owned()),
+    };
+    let script_name = match arguments.get(1) {
+        Some(cmd) => cmd,
+        None => return Err("provide the .psy script as the first argument to 'psy'".to_owned()),
+    };
+    let script = match PathBuf::from_str(script_name) {
+        Ok(pth) => pth,
+        Err(_) => return Err(format!("the script name '{}' is not a valid path syntactically", script_name)),
+    };
+    if !script.exists() {
+        return Err(format!("could not find script '{}'", script_name))
+    }
+    if !script.is_file() {
+        return Err(format!("script '{}' is not a file", script_name))
+    }
+    for i in 2 .. arguments.len() {
+        dbg!(&arguments[i]);  //TODO @mark: TEMPORARY! REMOVE THIS!
+    }
+    Ok(Params {
+        command,
+        script,
+        bindings: HashMap::new(),
+    })
+}
